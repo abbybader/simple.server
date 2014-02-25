ba3c300c6a9ec489df253af17389c571fcdaec5c
Dan D. <ddanny@gmail.com>
2014-02-25 19:44:22 +0200

    Added ExtJS and a simple stub.

---
+++ simple.server/webapp/js/MainViewport.js
@@ -0,0 +1,62 @@
+/* global Ext, window */
+
+Ext.onReady(function() {
+      var fileMenu = new Ext.menu.Menu({
+            items : []
+          });
+
+      var helpMenu = new Ext.menu.Menu({
+            items : [{
+                  text : 'Docs',
+                  handler : function() {
+                    var newWindow = window.open('', '_blank');
+                    newWindow.location = '/docs';
+                  }
+                }]
+          });
+
+      var toolbar = new Ext.toolbar.Toolbar(Ext.apply({
+            vertical : false,
+            items : [{
+                  text : 'File',
+                  arrowCls : '',
+                  tooltip : {
+                    title : 'File'
+                  },
+                  menu : fileMenu
+                }, {
+                  xtype : 'tbfill'
+                }, {
+                  xtype : 'tbseparator'
+                }, {
+                  text : 'Help',
+                  arrowCls : '',
+                  menu : helpMenu
+                }]
+          }));
+
+      var toolbarPanel = {
+        id : 'toolbar-panel',
+        region : 'north',
+        layout : 'card',
+        height : 30,
+        activeItem : 0,
+        border : true,
+        items : [toolbar]
+      };
+      
+      var contentPanel = new Ext.panel.Panel({
+            split : true,
+            frame : false,
+            region : 'center',
+            items : []
+          });
+
+      Ext.create('Ext.container.Viewport', {
+            id : 'mainViewport',
+            layout : 'border',
+            items : [toolbarPanel, contentPanel],
+            renderTo : Ext.getBody()
+          });
+
+    });
