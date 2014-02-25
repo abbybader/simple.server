4e21b828db639fd7bb0810b0d4cfd59b03f41b9b
Dan D. <ddanny@gmail.com>
2014-02-25 17:20:19 +0200

    Added a handler for serving static content.

--- simple.server/src/main/java/simple/server/SimpleServer.java
+++ simple.server/src/main/java/simple/server/SimpleServer.java
@@ -4,26 +4,37 @@ import java.util.EnumSet;
 
 import javax.servlet.DispatcherType;
 
+import org.eclipse.jetty.server.Handler;
 import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.server.handler.HandlerList;
+import org.eclipse.jetty.server.handler.ResourceHandler;
 import org.eclipse.jetty.servlet.ServletContextHandler;
 
 import com.google.inject.servlet.GuiceFilter;
 
 public class SimpleServer {
 
-    /**
-     * Simple backend for a To Do app
-     * @param args { httpPort } 
-     * @throws Exception
-     */
+	/**
+	 * Simple backend for a To Do app
+	 * 
+	 * @param args
+	 *            { httpPort }
+	 * @throws Exception
+	 */
 	public static void main(String[] args) throws Exception {
-	    int port = getPort(args);
+		int port = getPort(args);
 		Server server = new Server(port);
-		
+
 		ServletContextHandler context = new ServletContextHandler(ServletContextHandler.SESSIONS);
 		context.setContextPath("/");
-		server.setHandler(context);
-		
+
+		ResourceHandler resourceHandler = new ResourceHandler();
+		resourceHandler.setDirectoriesListed(true);
+		resourceHandler.setResourceBase("webapp");
+		HandlerList handlers = new HandlerList();
+		handlers.setHandlers(new Handler[] { resourceHandler, context });
+		server.setHandler(handlers);
+
 		context.addEventListener(new GuiceConfig());
 		context.addFilter(GuiceFilter.class, "/*", EnumSet.of(DispatcherType.REQUEST));
 		context.addServlet(EmptyServlet.class, "/*");
@@ -31,8 +42,8 @@ public class SimpleServer {
 		server.start();
 	}
 
-    private static int getPort(String[] args) {
-        return args.length > 0 ? Integer.valueOf(args[0]) : 1500; 
-    }
-	
+	private static int getPort(String[] args) {
+		return args.length > 0 ? Integer.valueOf(args[0]) : 1500;
+	}
+
 }
